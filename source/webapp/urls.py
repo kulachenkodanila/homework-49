744605662586fa7a60702b20733ace366aee51ec
Kulachenko_Danila <kulachenkodanila@gmail.com>
2022-07-11 15:43:37 +0300

    Создал класс изменения задачи, зарегестрировал в urls, создал update.html

--- source/webapp/urls.py
+++ source/webapp/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
 from django.views.generic import TemplateView, RedirectView
 
-from webapp.views import IndexView, WorkView
+from webapp.views import IndexView, WorkView, UpdateWork
 
 urlpatterns = [
     path('', IndexView.as_view(), name="index"),
     path('work/<int:pk>/', WorkView.as_view(), name="work_view"),
+    path('work/<int:pk>/update/', UpdateWork.as_view(), name="update_work"),
 
 ]
--- source/webapp/views.py
+++ source/webapp/views.py
@@ -5,6 +5,7 @@ from django.shortcuts import render
 from django.views import View
 from django.views.generic import TemplateView
 
+from webapp.forms import WorkForm
 from webapp.models import Work
 
 
@@ -23,3 +24,30 @@ class WorkView(TemplateView):
         work = get_object_or_404(Work, pk=pk)
         kwargs["work"] = work
         return super().get_context_data(**kwargs)
+
+
+class UpdateWork(View):
+    def get(self, request, *args, **kwargs):
+        pk = kwargs.get("pk")
+        work = get_object_or_404(Work, pk=pk)
+        if request.method == "GET":
+            form = WorkForm(initial={
+                "summary": work.summary,
+                "description": work.description,
+                "status": work.status,
+                "type": work.type
+            })
+            return render(request, "update.html", {"form": form})
+
+    def post(self, request, *args, **kwargs):
+        pk = kwargs.get("pk")
+        work = get_object_or_404(Work, pk=pk)
+        form = WorkForm(data=request.POST)
+        if form.is_valid():
+            work.summary = form.cleaned_data.get("summary")
+            work.description = form.cleaned_data.get("description")
+            work.status = form.cleaned_data.get("status")
+            work.type = form.cleaned_data.get("type")
+            work.save()
+            return redirect("work_view", pk=work.pk)
+        return render(request, "update.html", {"form": form})
