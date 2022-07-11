e59089d0f133afd9f7d6653b7f7f698c1a24b1a1
Kulachenko_Danila <kulachenkodanila@gmail.com>
2022-07-11 16:44:27 +0300

    Создал класс добавления задачи, зарегестрировал в urls, создал create.html

--- source/webapp/urls.py
+++ source/webapp/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path
 from django.views.generic import TemplateView, RedirectView
 
-from webapp.views import IndexView, WorkView, UpdateWork
+from webapp.views import IndexView, WorkView, UpdateWork, CreateWork
 
 urlpatterns = [
     path('', IndexView.as_view(), name="index"),
     path('work/<int:pk>/', WorkView.as_view(), name="work_view"),
     path('work/<int:pk>/update/', UpdateWork.as_view(), name="update_work"),
+    path('work/add/', CreateWork.as_view(), name="create_work"),
 
 ]
--- source/webapp/views.py
+++ source/webapp/views.py
@@ -27,27 +27,47 @@ class WorkView(TemplateView):
 
 
 class UpdateWork(View):
-    def get(self, request, *args, **kwargs):
+
+    def dispatch(self, request, *args, **kwargs):
         pk = kwargs.get("pk")
-        work = get_object_or_404(Work, pk=pk)
+        self.work = get_object_or_404(Work, pk=pk)
+        return super().dispatch(request, *args, **kwargs)
+
+    def get(self, request, *args, **kwargs):
         if request.method == "GET":
             form = WorkForm(initial={
-                "summary": work.summary,
-                "description": work.description,
-                "status": work.status,
-                "type": work.type
+                "summary": self.work.summary,
+                "description": self.work.description,
+                "status": self.work.status,
+                "type": self.work.type
             })
             return render(request, "update.html", {"form": form})
 
     def post(self, request, *args, **kwargs):
-        pk = kwargs.get("pk")
-        work = get_object_or_404(Work, pk=pk)
         form = WorkForm(data=request.POST)
         if form.is_valid():
-            work.summary = form.cleaned_data.get("summary")
-            work.description = form.cleaned_data.get("description")
-            work.status = form.cleaned_data.get("status")
-            work.type = form.cleaned_data.get("type")
-            work.save()
-            return redirect("work_view", pk=work.pk)
+            self.work.summary = form.cleaned_data.get("summary")
+            self.work.description = form.cleaned_data.get("description")
+            self.work.status = form.cleaned_data.get("status")
+            self.work.type = form.cleaned_data.get("type")
+            self.work.save()
+            return redirect("work_view", pk=self.work.pk)
         return render(request, "update.html", {"form": form})
+
+
+class CreateWork(View):
+    def get(self, request, *args, **kwargs):
+        if request.method == "GET":
+            form = WorkForm()
+            return render(request, "create.html", {"form": form})
+
+    def post(self, request, *args, **kwargs):
+        form = WorkForm(data=request.POST)
+        if form.is_valid():
+            summary = form.cleaned_data.get("summary")
+            description = form.cleaned_data.get("description")
+            status = form.cleaned_data.get("status")
+            type = form.cleaned_data.get("type")
+            new_work = Work.objects.create(summary=summary, description=description, status=status, type=type)
+            return redirect("work_view", pk=new_work.pk)
+        return render(request, "create.html", {"form": form})
