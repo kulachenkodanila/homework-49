1070fed7f640ed1039407b8f5e25f921546d42f8
Kulachenko_Danila <kulachenkodanila@gmail.com>
2022-07-14 00:59:39 +0300

    В формы добавил два метода валидации для описания и заголовка как указано было в задании (не более 100 символов)

--- source/webapp/forms.py
+++ source/webapp/forms.py
@@ -1,4 +1,5 @@
 from django import forms
+from django.core.exceptions import ValidationError
 from django.forms import widgets
 
 from webapp.models import Work
@@ -11,3 +12,17 @@ class WorkForm(forms.ModelForm):
         widgets = {
             "types": widgets.CheckboxSelectMultiple
         }
+
+    def clean_description(self):
+        description = self.cleaned_data.get('description')
+        if len(description) > 100:
+            raise ValidationError("Длина описания должна быть меньше 100 символов")
+        return description
+
+    def clean_summary(self):
+        summary = self.cleaned_data.get('description')
+        if len(summary) > 100:
+            raise ValidationError("Длина заголовка должна быть меньше 100 символов")
+        return summary
+
+
