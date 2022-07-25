ea63a82fb0acc9dcfb811a4b54fc477f96e4ee4b
Kulachenko_Danila <kulachenkodanila@gmail.com>
2022-07-26 01:03:33 +0300

    Добавил класс Project в models.py, зарегистрировал его в admin.py. Добавил несколько позиций в project

--- source/webapp/admin.py
+++ source/webapp/admin.py
@@ -1,19 +1,21 @@
 from django.contrib import admin
 
-from webapp.models import Work, Type, Status
+from webapp.models import Work, Type, Status, Project
 
 
 class WorkAdmin(admin.ModelAdmin):
-    list_display = ['id', 'summary', 'description', 'status', 'created_at', 'updated_at']
+    list_display = ['id', 'summary', 'description', 'status', 'created_at', 'updated_at', 'projects']
     list_filter = ['status', 'created_at']
+    list_display_links = ['summary']
     search_fields = ['description', 'status', 'types']
-    fields = ['summary', 'description', 'status', 'types', 'created_at']
+    fields = ['summary', 'description', 'status', 'types', 'created_at', 'projects']
     readonly_fields = ['updated_at', 'created_at']
 
 
 admin.site.register(Work, WorkAdmin),
 admin.site.register(Type)
 admin.site.register(Status)
+admin.site.register(Project)
 
 
 # Register your models here.
--- source/webapp/models.py
+++ source/webapp/models.py
@@ -37,10 +37,12 @@ class Type(BaseModel):
 
 class Work(BaseModel):
     summary = models.CharField(max_length=100, null=False, blank=False, verbose_name="Заголовок")
-    description = models.TextField(max_length=100, blank=True, verbose_name="Описание")
+    description = models.TextField(max_length=100, null=True, blank=True, verbose_name="Описание")
     status = models.ForeignKey("webapp.Status", on_delete=models.CASCADE, related_name="Statuses",
                                verbose_name="Статус")
     types = models.ManyToManyField("webapp.Type", related_name="works", blank=True)
+    projects = models.ForeignKey("webapp.Project", on_delete=models.CASCADE, default=1, related_name="Projects",
+                               verbose_name="Проект")
 
     def __str__(self):
         return f"{self.pk}. {self.description} - {self.status} - {self.summary} - {self.types}"
@@ -49,5 +51,19 @@ class Work(BaseModel):
         db_table = "works"
         verbose_name = "Задача"
         verbose_name_plural = "Задачи"
+
+class Project(BaseModel):
+    start_date = models.DateField(verbose_name="Дата начала")
+    finish_date = models.DateField(verbose_name="Дата окончания")
+    name = models.CharField(max_length=30, verbose_name="Название")
+    description_project = models.TextField(max_length=150, blank=True, verbose_name="Описание")
+
+    def __str__(self):
+        return f"{self.pk}. {self.start_date} - {self.finish_date} - {self.name} - {self.description_project}"
+
+    class Meta:
+        db_table = "projects"
+        verbose_name = "Проект"
+        verbose_name_plural = "Проекты"
 
 # Create your models here.
